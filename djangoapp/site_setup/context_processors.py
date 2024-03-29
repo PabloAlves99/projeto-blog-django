dc0d474cbed3f3ee9a2de4e67d40753de4ca2755
Pablo <PabloHAlves99@gmail.com>
2024-03-29 10:52:43 -0300

    Criando context_processors

---
+++ djangoapp/site_setup/context_processors.py
@@ -0,0 +1,16 @@
+from site_setup.models import SiteSetup
+
+
+def context_processor_example(request):
+    return {
+        'example': 'Criação de exemplo'
+    }
+
+
+def site_setup(request):
+    setup = SiteSetup.objects.order_by('-id').first()
+    return {
+        'site_setup': {
+            'title': setup
+        }
+    }
