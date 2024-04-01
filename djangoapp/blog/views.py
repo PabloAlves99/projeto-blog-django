0f733352363e1e5d5a069f678bb1a041798ff09f
Pablo <PabloHAlves99@gmail.com>
2024-04-01 18:02:53 -0300

    Completando a page

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,4 +1,4 @@
-from blog.models import Post
+from blog.models import Post, Page
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render
@@ -94,12 +94,12 @@ def search(request):
 
 
 def page(request, slug):
-
+    _page = (Page.objects.filter(is_published=True).filter(slug=slug)).first()
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+            'page': _page,
         }
     )
 
